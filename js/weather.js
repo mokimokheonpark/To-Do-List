8272483ebb4f82c8f35e783ddd7c4e0e88deba6f
Mokheon Park <mokimokheonpark@gmail.com>
2023-04-20 00:46:05 -0400

    third commit

--- js/weather.js
+++ js/weather.js
@@ -7,10 +7,9 @@ function handleSuccess(position) {
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child")
         const city = document.querySelector("#weather span:last-child")
+        weather.innerText = `${data.weather[0].main}, ${data.main.temp}°C,`;
         city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
-
 }
 
 function handleError() {
